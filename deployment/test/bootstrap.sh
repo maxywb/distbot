7176a595c3ff5619166870dd79c2478e7a69c8c2
max beckett <beckett.max@gmail.com>
2016-10-14 06:26:20 -0400

    refactor provision script, add config blobs, start services script

--- deployment/test/bootstrap.sh
+++ deployment/test/bootstrap.sh
@@ -4,6 +4,8 @@ add_user() {
     username=$1
     home=/opt/${username}
 
+    echo "$(date) add user ${username}"
+
     useradd -d ${home} ${username}
     cp -f /vagrant/configs/bashrc ${home}/.bashrc
 
@@ -16,30 +18,33 @@ add_user() {
     chmod 600 ${home}/.ssh/authorized_keys
     chmod 700 ${home}/.ssh
     chown -R ${username}:${username} ${home}
-    
+    popd    
+
     su - ${username} -c "ssh-keyscan -H localhost >> ${home}/.ssh/known_hosts"
+    su - ${username} -c "ssh-keyscan -H 0.0.0.0 >> ${home}/.ssh/known_hosts"
     su - ${username} -c "ssh-keyscan -H $(hostname) >> ${home}/.ssh/known_hosts"
 
-    popd
+    usermod --append --groups supergroup ${username}
 }
 
 install_package() {
     source=$1
     destination=$2
     url=$3
+    
+    echo "$(date) installing ${source}"
 
-    echo "curl  -o ${source}.tar.gz ${url}"
     curl  --silent -o ${source}.tar.gz ${url}
 
-    echo "tar zxf ${source}.tar.gz"
     tar zxf ${source}.tar.gz
 
-    echo "mkdir -p ${destination}"
     mkdir -p ${destination}
     
-    echo "cp -r ${source}/* ${destination}"
     cp -r ${source}/* ${destination}
 }
+
+# make supergroup
+groupadd supergroup
 
 # make users
 add_user "hadoop"
@@ -68,12 +73,12 @@ cp -f /vagrant/configs/yarn-site.xml /opt/hadoop/etc/hadoop/yarn-site.xml
 
 echo "export JAVA_HOME=/usr/java/default" >> /opt/hadoop/etc/hadoop/hadoop-env.sh
 
-chown -R hadoop:hadoop /opt/hadoop
+chown -R hadoop:supergroup /opt/hadoop
 
 # install spark
 install_package spark-2.0.1-bin-hadoop2.7 /opt/spark http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
 
-chown -R spark:spark /opt/spark
+chown -R spark:supergroup /opt/spark
 
 # install hbase
 install_package hbase-1.2.3 /opt/hbase http://mirror.symnds.com/software/Apache/hbase/1.2.3/hbase-1.2.3-bin.tar.gz
@@ -82,12 +87,14 @@ cp -f /vagrant/configs/hbase-site.xml /opt/hbase/conf/hbase-site.xml
 
 echo "export JAVA_HOME=/usr/java/default" >> /opt/hbase/conf/hbase-env.sh
 
-chown -R hbase:hbase /opt/hbase
+chown -R hbase:supergroup /opt/hbase
 
 # install zookeeper
-install_package zookeeper-3.4.9 /opt/zookeeper  http://www.gtlib.gatech.edu/pub/apache/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
+install_package zookeeper-3.4.9 /opt/zookeeper http://www.gtlib.gatech.edu/pub/apache/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
 
-chown -R zookeeper:zookeeper /opt/zookeeper
+cp -f /vagrant/configs/zoo.cfg /opt/zookeeper/conf/zoo.cfg
+
+chown -R zookeeper:supergroup /opt/zookeeper
 
 # done installing packages
 popd #/tmp
@@ -97,5 +104,9 @@ popd #/tmp
 echo "10.0.0.201 node-1" >> /etc/hosts
 echo "10.0.0.202 node-2" >> /etc/hosts
 
+# set permissions on /spam
+chown -R root:supergroup /spam
+chmod -R g+rw /spam
 
+# done
 date >> /spam/provision_timestamp
---
+++ deployment/test/start_services.sh
@@ -0,0 +1,19 @@
+set -e
+
+run_as_user() {
+    username=$1
+    command=$2
+
+    su - ${username} -c ${command}
+}
+
+
+
+# start hdfs
+run_as_user "hadoop" "start-all.sh"
+
+# start zookeeper
+run_as_user "zookeeper" "zkServer.sh start"
+
+# start hbase
+run_as_user "hbase" "start-hbase.sh"
