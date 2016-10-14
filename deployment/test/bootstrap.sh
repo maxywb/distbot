f2047847f313f0d74672f5c9516fcbedc7029fce
max beckett <beckett.max@gmail.com>
2016-10-13 20:08:07 -0400

    refactor provisioning script

--- deployment/test/bootstrap.sh
+++ deployment/test/bootstrap.sh
@@ -1,44 +1,52 @@
 set -e 
 
-# make hadoop user
-useradd -d /opt/hadoop hadoop
-echo "hadoop" | passwd --stdin hadoop
-mkdir /opt/hadoop/.ssh
+add_user() {
+    username=$1
+    home=/opt/${username}
 
-pushd /opt/hadoop/.ssh
-ssh-keygen -b 1024 -f id_dsa -t dsa -q -N ""
-cat id_dsa.pub | sudo tee -a /opt/hadoop/.ssh/authorized_keys  
-chmod 600 /opt/hadoop/.ssh/authorized_keys
-chmod 700 /opt/hadoop/.ssh
-popd
-# chown later after hadoop stuff is put here
+    useradd -d ${home} ${username}
+    cp -f /vagrant/configs/bashrc ${home}/.bashrc
 
-# make spark user
-useradd -d /opt/spark spark
-echo "spark" | passwd --stdin spark
-mkdir /opt/spark/.ssh
+    echo "${username}" | passwd --stdin ${username}
+    mkdir ${home}/.ssh
 
-pushd /opt/spark/.ssh
-ssh-keygen -b 1024 -f id_dsa -t dsa -q -N ""
-cat id_dsa.pub | sudo tee -a /opt/spark/.ssh/authorized_keys  
-chmod 600 /opt/spark/.ssh/authorized_keys
-chmod 700 /opt/spark/.ssh
-popd
-# chown later after spark stuff is put here
+    pushd ${home}/.ssh
+    ssh-keygen -b 1024 -f id_dsa -t dsa -q -N ""
+    cat id_dsa.pub > authorized_keys
+    chmod 600 ${home}/.ssh/authorized_keys
+    chmod 700 ${home}/.ssh
+    chown -R ${username}:${username} ${home}
+    
+    su - ${username} -c "ssh-keyscan -H localhost >> ${home}/.ssh/known_hosts"
+    su - ${username} -c "ssh-keyscan -H $(hostname) >> ${home}/.ssh/known_hosts"
 
-# make spam user
-useradd spam
-echo "spam" | passwd --stdin spam
-mv /home/vagrant/spam/ /home/spam
-mkdir /home/spam/.ssh
+    popd
+}
 
-pushd /home/spam/.ssh
-ssh-keygen -b 1024 -f id_dsa -t dsa -q -N ""
-cat id_dsa.pub | sudo tee -a /home/spam/.ssh/authorized_keys  
-chmod 600 /home/spam/.ssh/authorized_keys
-chmod 700 /home/spam/.ssh
-popd
-chown -R spam:spam /home/spam
+install_package() {
+    source=$1
+    destination=$2
+    url=$3
+
+    echo "curl  -o ${source}.tar.gz ${url}"
+    curl  --silent -o ${source}.tar.gz ${url}
+
+    echo "tar zxf ${source}.tar.gz"
+    tar zxf ${source}.tar.gz
+
+    echo "mkdir -p ${destination}"
+    mkdir -p ${destination}
+    
+    echo "cp -r ${source}/* ${destination}"
+    cp -r ${source}/* ${destination}
+}
+
+# make users
+add_user "hadoop"
+add_user "hbase"
+add_user "spam"
+add_user "spark"
+add_user "zookeeper"
 
 # install packages
 pushd /tmp
@@ -48,75 +56,46 @@ curl --silent -O -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" 
 rpm -U jdk-8u102-linux-x64.rpm
 
 # install scala
-curl --silent -O http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz -o scala-2.11.8.tgz
-tar zxf scala-2.11.8.tgz
-mv scala-2.11.8 /opt/scala
+install_package scala-2.11.8 /opt/scala http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
 
-echo <<EOF >> /opt/hadoop/.bashrc
-## add SCALA to path
-export PATH=$PATH:/opt/scala/bin
-EOF
+# install hadoop 
+install_package hadoop-2.7.3 /opt/hadoop http://apache.javapipe.com/hadoop/common/stable/hadoop-2.7.3.tar.gz
 
-echo <<EOF >> /opt/spark/.bashrc
-## add SCALA to path
-export PATH=$PATH:/opt/scala/bin
-EOF
+cp -f /vagrant/configs/core-site.xml /opt/hadoop/etc/hadoop/core-site.xml
+cp -f /vagrant/configs/hdfs-site.xml /opt/hadoop/etc/hadoop/hdfs-site.xml
+cp -f /vagrant/configs/mapred-site.xml /opt/hadoop/etc/hadoop/mapred-site.xml
+cp -f /vagrant/configs/yarn-site.xml /opt/hadoop/etc/hadoop/yarn-site.xml
 
-# setup hadoop 
-curl --silent -O http://apache.javapipe.com/hadoop/common/stable/hadoop-2.7.3.tar.gz
-tar zxf hadoop-2.7.3.tar.gz
-cp -r hadoop-2.7.3/* /opt/hadoop
-
-echo <<EOF >> /opt/hadoop/.bashrc
-## JAVA env variables
-export JAVA_HOME=/usr/java/default
-export PATH=$PATH:$JAVA_HOME/bin
-export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
-## HADOOP env variables
-export HADOOP_HOME=/opt/hadoop
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_YARN_HOME=$HADOOP_HOME
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-EOF
-
-echo <<EOF >> /opt/spark/.bashrc
-## JAVA env variables
-export JAVA_HOME=/usr/java/default
-export PATH=$PATH:$JAVA_HOME/bin
-export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
-## HADOOP env variables
-export HADOOP_HOME=/opt/hadoop
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_YARN_HOME=$HADOOP_HOME
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-EOF
+echo "export JAVA_HOME=/usr/java/default" >> /opt/hadoop/etc/hadoop/hadoop-env.sh
 
 chown -R hadoop:hadoop /opt/hadoop
 
 # install spark
-curl --silent -O http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
-tar zxf spark-2.0.1-bin-hadoop2.7.tgz
-cp -r spark-2.0.1-bin-hadoop2.7/* /opt/spark 
-
-echo <<EOF >> /opt/spark/.bashrc
-## add SPARK to path
-export PATH=$PATH:/opt/spark/bin
-EOF
+install_package spark-2.0.1-bin-hadoop2.7 /opt/spark http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1-bin-hadoop2.7.tgz
 
 chown -R spark:spark /opt/spark
+
+# install hbase
+install_package hbase-1.2.3 /opt/hbase http://mirror.symnds.com/software/Apache/hbase/1.2.3/hbase-1.2.3-bin.tar.gz
+
+cp -f /vagrant/configs/hbase-site.xml /opt/hbase/conf/hbase-site.xml
+
+echo "export JAVA_HOME=/usr/java/default" >> /opt/hbase/conf/hbase-env.sh
+
+chown -R hbase:hbase /opt/hbase
+
+# install zookeeper
+install_package zookeeper-3.4.9 /opt/zookeeper  http://www.gtlib.gatech.edu/pub/apache/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
+
+chown -R zookeeper:zookeeper /opt/zookeeper
+
+# done installing packages
+popd #/tmp
 
 # set hostnames
 # TODO: this is fuckin gross, proper fix is DNS
 echo "10.0.0.201 node-1" >> /etc/hosts
 echo "10.0.0.202 node-2" >> /etc/hosts
 
-# done installing packages
-popd #/tmp
+
+date >> /spam/provision_timestamp
