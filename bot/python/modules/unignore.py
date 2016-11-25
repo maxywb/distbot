a7bc8ed0dcbd4d6f3934c0735dc5201f1ab5ca91
max beckett <beckett.max@gmail.com>
2016-11-25 15:10:34 -0500

    unignore command

---
+++ bot/python/modules/unignore.py
@@ -0,0 +1,22 @@
+import util.admin
+import util.message 
+
+MODULE_CLASS_NAME="Unignore"
+
+MODULE_SUBCOMMAND="unignore"
+
+class Unignore():
+    HELP_TEXT = ""
+
+    def __init__(self, configuration, zk_client, **kwargs):
+        self.configuration = configuration
+        self.zk_client = zk_client
+
+    def consume(self, message):
+        if not util.admin.is_admin(self.zk_client, self.configuration, message):
+            return util.admin.admonish(self.zk_client, self.configuration, message)
+
+        for user in message["message"].command[1:]:
+            self.configuration.bad_users.discard(user)
+        
+        
