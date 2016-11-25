c6662f1e99fe3908bc1ad1f26d4dc837cace4498
max beckett <beckett.max@gmail.com>
2016-11-25 14:47:53 -0500

    privmsg command

---
+++ bot/python/modules/privmsg.py
@@ -0,0 +1,27 @@
+import kazoo.recipe.watchers as kzw
+
+import util.admin
+import util.message 
+
+MODULE_CLASS_NAME="Privmsg"
+
+MODULE_SUBCOMMAND="privmsg"
+
+class Privmsg():
+    HELP_TEXT = ""
+
+    def __init__(self, configuration, zk_client, **kwargs):
+        self.configuration = configuration
+        self.zk_client = zk_client
+
+    def consume(self, message):
+        if not util.admin.is_admin(self.zk_client, self.configuration, message):
+            return util.admin.admonish(self.zk_client, self.configuration, message)
+
+        pieces = message["message"].command
+        who = pieces[1]
+        where = "PRIVMSG"
+        text = " ".join(pieces[2:])
+
+        return util.message.Message(who, where, text)
+
